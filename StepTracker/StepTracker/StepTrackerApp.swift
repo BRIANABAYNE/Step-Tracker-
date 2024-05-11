393ad050827d1c2f14f418975f813146f4878e50
Briana Bayne <135153210+BRIANABAYNE@users.noreply.github.com>
2024-05-11 13:05:07 -0600

    Created Xcode Project

---
+++ StepTracker/StepTracker/StepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepTrackerApp.swift
+//  StepTracker
+//
+//  Created by Briana Bayne on 5/11/24.
+//
+
+import SwiftUI
+
+@main
+struct StepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
