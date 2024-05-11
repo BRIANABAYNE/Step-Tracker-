f7e6cb621476c56a38bdf0eae1a9faf2839b3de0
Briana Bayne <135153210+BRIANABAYNE@users.noreply.github.com>
2024-05-11 13:54:54 -0600

    init dashboard

--- StepTracker/StepTracker/ContentView.swift
+++ StepTracker/StepTracker/ContentView.swift
@@ -9,13 +9,63 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        /// Navigation Stack - View that displays a root view and enables you to present additional views over the root view.
+        NavigationStack {
+            ScrollView {
+                /// VStack - A view that arranges it's subview in a vertical line ^
+                VStack(spacing: 20) {
+                    VStack {
+                        /// View that arranges it's subviews in a horizontal line ---------
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Label("Steps", systemImage: "figure.walk")
+                                    .font(.title3.bold())
+                                    .foregroundStyle(.pink)
+                                Text("Avg: 10K Steps")
+                                    .font(.caption)
+                                    .foregroundStyle(.secondary)
+                            }
+                            
+                            Spacer()
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.secondary)
+                        }
+                        .padding(.bottom, 12)
+                        
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundStyle(.secondary)
+                            .frame(height: 150)
+                    }
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+                }
+                
+                VStack(alignment:.leading) {
+                    VStack {
+                        /// View that arranges it's subviews in a horizontal line ---------
+                        VStack(alignment: .leading) {
+                            Label("Averages", systemImage: "calendar")
+                                .font(.title3.bold())
+                                .foregroundStyle(.pink)
+                            Text("Last 28 Days")
+                                .font(.caption)
+                                .foregroundStyle(.secondary)
+                        }
+                      
+                        .padding(.bottom, 12)
+                        
+                        RoundedRectangle(cornerRadius: 12)
+                            .foregroundStyle(.secondary)
+                            .frame(height: 240)
+                    }
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+                }
+                
+            }
+            .padding()
+            .navigationTitle("Dashboard")
         }
-        .padding()
     }
 }
 
