603b7c82075d56d1103623c2095ec48809f93abd
Briana Bayne <135153210+BRIANABAYNE@users.noreply.github.com>
2024-05-12 14:42:26 -0600

    Navigation Setup

--- StepTracker/StepTracker/ContentView.swift
+++ StepTracker/StepTracker/ContentView.swift
@@ -7,35 +7,78 @@
 
 import SwiftUI
 
+/// CaseIterable - allows to run through all cases like an array
+/// Identifiable - For each
+
+
+// MARK: - Enum
+
+enum HealthMetricContext: CaseIterable, Identifiable {
+    case steps, weight
+    /// Self = the HealthMetricContext
+    var id: Self { return self }
+    
+    var title: String {
+        switch self {
+        case .steps:
+            return "Steps"
+        case .weight:
+            return "Weight"
+        }
+    }
+}
+
+
 struct ContentView: View {
+    
+    // MARK: - Properties
+    
+    @State private var selectedStat: HealthMetricContext = .steps
+    
+    // MARK: - Computed Property
+    
+    var isSteps: Bool {
+        selectedStat == .steps
+    }
     var body: some View {
         /// Navigation Stack - View that displays a root view and enables you to present additional views over the root view.
         NavigationStack {
             ScrollView {
                 /// VStack - A view that arranges it's subview in a vertical line ^
                 VStack(spacing: 20) {
+                    /// Creates a title key
+                    Picker("Selected Stat", selection: $selectedStat) {
+                        /// Using for each with the Identifiable - Got all cases by CaseIterable
+                        ForEach(HealthMetricContext.allCases) { metric in
+                            Text(metric.title)
+                        }
+                    }
+                    .pickerStyle(.segmented)
                     VStack {
                         /// View that arranges it's subviews in a horizontal line ---------
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Label("Steps", systemImage: "figure.walk")
-                                    .font(.title3.bold())
-                                    .foregroundStyle(.pink)
-                                Text("Avg: 10K Steps")
-                                    .font(.caption)
-                                    .foregroundStyle(.secondary)
+                        ///  Navigation - When tapped will trigger the navigation of the screen
+                        NavigationLink(value: selectedStat) {
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Label("Steps", systemImage: "figure.walk")
+                                        .font(.title3.bold())
+                                        .foregroundStyle(.pink)
+                                    Text("Avg: 10K Steps")
+                                        .font(.caption)
+                                }
+                                
+                                Spacer()
+                                Image(systemName: "chevron.right")
                             }
-                            
-                            Spacer()
-                            Image(systemName: "chevron.right")
-                                .foregroundColor(.secondary)
                         }
                         .padding(.bottom, 12)
                         
                         RoundedRectangle(cornerRadius: 12)
                             .foregroundStyle(.secondary)
                             .frame(height: 150)
+                        
                     }
+                    .foregroundStyle(.secondary)
                     .padding()
                     .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
                 }
@@ -51,7 +94,7 @@ struct ContentView: View {
                                 .font(.caption)
                                 .foregroundStyle(.secondary)
                         }
-                      
+                        
                         .padding(.bottom, 12)
                         
                         RoundedRectangle(cornerRadius: 12)
@@ -65,7 +108,12 @@ struct ContentView: View {
             }
             .padding()
             .navigationTitle("Dashboard")
+            .navigationDestination(for: HealthMetricContext.self)  { metric in
+                Text(metric.title)
+            }
         }
+        /// Ternary Operator
+        .tint(isSteps ? .pink : .indigo)
     }
 }
 
