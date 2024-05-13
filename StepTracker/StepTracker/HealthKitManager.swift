d1dd379df8a931d45e97c51da9c8b4b6de2487a8
Briana Bayne <135153210+BRIANABAYNE@users.noreply.github.com>
2024-05-13 15:30:49 -0600

    Trying to figure out where half my code went

---
+++ StepTracker/StepTracker/HealthKitManager.swift
@@ -0,0 +1,16 @@
+//
+//  HealthKitManager.swift
+//  StepTracker
+//
+//  Created by Briana Bayne on 5/13/24.
+//
+
+import Foundation
+import HealthKit
+import Observation
+
+@Observable class HealthKitManager {
+    
+    let hkManager = HealthKitManager()
+    let store = HKHealthStore()
+}
--- StepTracker/StepTracker/StepTrackerApp.swift
+++ StepTracker/StepTracker/StepTrackerApp.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 @main
 struct StepTrackerApp: App {
+   
+    let hkManager = HealthKitManager()
+    
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+//           DashboardView()
+//                .environment(hkManager)
         }
     }
 }
